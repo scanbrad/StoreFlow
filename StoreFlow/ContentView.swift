576cf31b708901751d16b86b74ae1b4ef51cb234
Brad Scott <bscott22@mac.com>
2025-10-30 21:11:49 -0400

    Initial Commit

---
+++ StoreFlow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StoreFlow
+//
+//  Created by Brad Scott on 10/30/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StoreFlow/StoreFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoreFlowApp.swift
+//  StoreFlow
+//
+//  Created by Brad Scott on 10/30/25.
+//
+
+import SwiftUI
+
+@main
+struct StoreFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
